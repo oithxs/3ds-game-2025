59e42090537cb90ae771d639a35b3cd226f47f8a
YutoMaeda <55312129+YutoMaeda1209@users.noreply.github.com>
2025-10-28 06:55:46 +0000

    Update main.c
    フィールド初期化＆描画処理

--- source/main.c
+++ source/main.c
@@ -3,26 +3,66 @@
 #include <string.h>
 #include <3ds.h>
 
-int main(int argc, char* argv[])
-{
-	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+#define FIELD_HEIGHT 10
+#define FIELD_WIDTH 10
 
-	printf("Hello, world!\n");
+int field[FIELD_HEIGHT][FIELD_WIDTH];
 
-	// Main loop
-	while (aptMainLoop())
-	{
-		gspWaitForVBlank();
-		gfxSwapBuffers();
-		hidScanInput();
+int initField();
+int drawField();
 
-		// Your code goes here
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
-	}
+// メイン関数
+int main(int argc, char* argv[]) {
+    gfxInitDefault();
+    consoleInit(GFX_TOP, NULL);
 
-	gfxExit();
-	return 0;
+    initField();
+
+    while (aptMainLoop()) {
+        gspWaitForVBlank();
+        
+        gfxSwapBuffers();
+        hidScanInput();
+
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START)
+            break;
+        if (kDown & KEY_A)
+            field[5][5] = 1;
+
+        drawField();
+    }
+
+    gfxExit();
+    return 0;
+}
+
+// フィールドを初期化する関数
+int initField() {
+    for (int y = 0; y < FIELD_HEIGHT; y++) {
+        for (int x = 0; x < FIELD_WIDTH; x++) {
+            if (x == 0 || x == FIELD_WIDTH - 1
+                || y == 0 || y == FIELD_HEIGHT - 1)
+                field[y][x] = 1;
+            else
+                field[y][x] = 0;
+        }
+    }
+    return 0;
+}
+
+// フィールドを描画する関数
+int drawField() {
+    consoleClear();
+
+    for (int y = 0; y < FIELD_HEIGHT; y++) {
+        for (int x = 0; x < FIELD_WIDTH; x++) {
+            if (field[y][x] == 1)
+                printf("#");
+            else if (field[y][x] == 0)
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
 }
