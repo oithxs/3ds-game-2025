80849575961f783e1eac719c135d714a1369182c
YutoMaeda <55312129+YutoMaeda1209@users.noreply.github.com>
2025-11-02 08:48:33 +0000

    Update main.c
    スネークゲームコードをCopilotに作成してもらった

--- source/main.c
+++ source/main.c
@@ -3,66 +3,200 @@
 #include <string.h>
 #include <3ds.h>
 
-#define FIELD_HEIGHT 10
-#define FIELD_WIDTH 10
+#define BOARD_WIDTH 40
+#define BOARD_HEIGHT 20
+#define MAX_SNAKE_LENGTH 100
 
-int field[FIELD_HEIGHT][FIELD_WIDTH];
+typedef struct {
+    int x, y;
+} Point;
 
-int initField();
-int drawField();
+typedef struct {
+    Point body[MAX_SNAKE_LENGTH];
+    int length;
+    int direction; // 0: up, 1: right, 2: down, 3: left
+} Snake;
 
-// メイン関数
-int main(int argc, char* argv[]) {
-    gfxInitDefault();
-    consoleInit(GFX_TOP, NULL);
+char board[BOARD_HEIGHT][BOARD_WIDTH];
+Snake snake;
+Point food;
+int score = 0;
+int gameRunning = 1;
 
-    initField();
-
-    while (aptMainLoop()) {
-        gspWaitForVBlank();
-        
-        gfxSwapBuffers();
-        hidScanInput();
-
-        u32 kDown = hidKeysDown();
-        if (kDown & KEY_START)
-            break;
-        if (kDown & KEY_A)
-            field[5][5] = 1;
-
-        drawField();
-    }
-
-    gfxExit();
-    return 0;
+void initGame() {
+    // Initialize board
+    memset(board, ' ', sizeof(board));
+    
+    // Initialize snake
+    snake.length = 3;
+    snake.direction = 1; // Start moving right
+    snake.body[0].x = 5;  snake.body[0].y = 10; // Head
+    snake.body[1].x = 4;  snake.body[1].y = 10;
+    snake.body[2].x = 3;  snake.body[2].y = 10; // Tail
+    
+    // Place initial food
+    food.x = 15;
+    food.y = 10;
+    
+    score = 0;
+    gameRunning = 1;
 }
 
-// フィールドを初期化する関数
-int initField() {
-    for (int y = 0; y < FIELD_HEIGHT; y++) {
-        for (int x = 0; x < FIELD_WIDTH; x++) {
-            if (x == 0 || x == FIELD_WIDTH - 1
-                || y == 0 || y == FIELD_HEIGHT - 1)
-                field[y][x] = 1;
-            else
-                field[y][x] = 0;
+void generateFood() {
+    int validPosition = 0;
+    while (!validPosition) {
+        food.x = rand() % (BOARD_WIDTH - 2) + 1;
+        food.y = rand() % (BOARD_HEIGHT - 2) + 1;
+        
+        validPosition = 1;
+        // Check if food spawns on snake
+        for (int i = 0; i < snake.length; i++) {
+            if (snake.body[i].x == food.x && snake.body[i].y == food.y) {
+                validPosition = 0;
+                break;
+            }
         }
     }
-    return 0;
 }
 
-// フィールドを描画する関数
-int drawField() {
-    consoleClear();
+void updateSnake() {
+    // Calculate new head position
+    Point newHead = snake.body[0];
+    
+    switch (snake.direction) {
+        case 0: newHead.y--; break; // Up
+        case 1: newHead.x++; break; // Right
+        case 2: newHead.y++; break; // Down
+        case 3: newHead.x--; break; // Left
+    }
+    
+    // Check wall collision
+    if (newHead.x <= 0 || newHead.x >= BOARD_WIDTH - 1 || 
+        newHead.y <= 0 || newHead.y >= BOARD_HEIGHT - 1) {
+        gameRunning = 0;
+        return;
+    }
+    
+    // Check self collision
+    for (int i = 0; i < snake.length; i++) {
+        if (snake.body[i].x == newHead.x && snake.body[i].y == newHead.y) {
+            gameRunning = 0;
+            return;
+        }
+    }
+    
+    // Move snake body
+    for (int i = snake.length - 1; i > 0; i--) {
+        snake.body[i] = snake.body[i - 1];
+    }
+    snake.body[0] = newHead;
+    
+    // Check food collision
+    if (newHead.x == food.x && newHead.y == food.y) {
+        score += 10;
+        if (snake.length < MAX_SNAKE_LENGTH - 1) {
+            snake.length++;
+        }
+        generateFood();
+    }
+}
 
-    for (int y = 0; y < FIELD_HEIGHT; y++) {
-        for (int x = 0; x < FIELD_WIDTH; x++) {
-            if (field[y][x] == 1)
-                printf("#");
-            else if (field[y][x] == 0)
-                printf(" ");
+void drawGame() {
+    consoleClear();
+    
+    // Clear board
+    memset(board, ' ', sizeof(board));
+    
+    // Draw borders
+    for (int x = 0; x < BOARD_WIDTH; x++) {
+        board[0][x] = '#';
+        board[BOARD_HEIGHT - 1][x] = '#';
+    }
+    for (int y = 0; y < BOARD_HEIGHT; y++) {
+        board[y][0] = '#';
+        board[y][BOARD_WIDTH - 1] = '#';
+    }
+    
+    // Draw snake
+    for (int i = 0; i < snake.length; i++) {
+        if (i == 0) {
+            board[snake.body[i].y][snake.body[i].x] = '@'; // Head
+        } else {
+            board[snake.body[i].y][snake.body[i].x] = 'O'; // Body
+        }
+    }
+    
+    // Draw food
+    board[food.y][food.x] = '*';
+    
+    // Print board
+    for (int y = 0; y < BOARD_HEIGHT; y++) {
+        for (int x = 0; x < BOARD_WIDTH; x++) {
+            printf("%c", board[y][x]);
         }
         printf("\n");
     }
+    
+    printf("\nScore: %d\n", score);
+    printf("D-Pad: Move  START: Quit\n");
+    
+    if (!gameRunning) {
+        printf("\nGAME OVER!\n");
+        printf("A: Restart  START: Quit\n");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    gfxInitDefault();
+    consoleInit(GFX_TOP, NULL);
+    
+    srand(svcGetSystemTick());
+    initGame();
+    
+    int frameCounter = 0;
+    
+    // Main loop
+    while (aptMainLoop())
+    {
+        gspWaitForVBlank();
+        hidScanInput();
+        
+        u32 kDown = hidKeysDown();
+        
+        if (kDown & KEY_START)
+            break; // Exit game
+        
+        if (gameRunning) {
+            // Handle input
+            if (kDown & KEY_DUP && snake.direction != 2) {
+                snake.direction = 0; // Up
+            } else if (kDown & KEY_DRIGHT && snake.direction != 3) {
+                snake.direction = 1; // Right
+            } else if (kDown & KEY_DDOWN && snake.direction != 0) {
+                snake.direction = 2; // Down
+            } else if (kDown & KEY_DLEFT && snake.direction != 1) {
+                snake.direction = 3; // Left
+            }
+            
+            // Update game logic every 10 frames (adjust for speed)
+            frameCounter++;
+            if (frameCounter >= 10) {
+                updateSnake();
+                frameCounter = 0;
+            }
+        } else {
+            // Game over state
+            if (kDown & KEY_A) {
+                initGame(); // Restart game
+                frameCounter = 0;
+            }
+        }
+        
+        drawGame();
+        gfxSwapBuffers();
+    }
+    
+    gfxExit();
     return 0;
 }
